31e39d4f6f61921dec47c08669f713cff444446c
masaki111111 <oit.e1921079@gmail.com>
2024-12-11 12:09:01 +0900

    Add files via upload

--- test1.py
+++ test1.py
@@ -132,8 +132,8 @@ duration_in_hrs = (a0["data"][0]["total_sleep_duration"])#変数に一日目の�
 # CSVファイルのパスを指定
 #csv_file_path = r"C:\Users\owner\OneDrive - 大阪工業大学\ウエルネス研究室\福田勝基\Core\22_08_2024_DA38DDB3C43F_history.csv"
 #csv_file_path = r"g: " + os.path.join("\マイドライブ\Test1", "file.csv"
-csv_file_path = r"C:\CORE\介入06-12_2024_DA38DDB3C43F_history.csv"
-
+# = r"C:\CORE\介入06-12_2024_DA38DDB3C43F_history.csv"
+csv_file_path = r'C:\CORE\介入06-12_2024_DA38DDB3C43F_history.csv'
 
 try:
     df = pd.read_csv(csv_file_path, sep = ';', header = 1,)
