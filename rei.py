ae16491271dbd712e02cc9c73d7ae45671d2d680
masaki111111 <oit.e1921079@gmail.com>
2025-01-11 18:03:26 +0900

    Update rei.py

--- rei.py
+++ rei.py
@@ -22,29 +22,10 @@ import streamlit.components.v1 as stc
 import base64
 import time
 
-#button = st.button('アプリ実行')
 
-#if button:
 
-audio_path1 = 'data/short-8bit-05.wav' #入力する音声ファイル
 
-audio_placeholder = st.empty()
 
-file_ = open(audio_path1, "rb")
-contents = file_.read()
-file_.close()
-
-audio_str = "data:audio/ogg;base64,%s"%(base64.b64encode(contents).decode())
-audio_html = """
-                <audio autoplay=True>
-                <source src="%s" type="audio/ogg" autoplay=True>
-                Your browser does not support the audio element.
-                </audio>
-            """ %audio_str
-
-audio_placeholder.empty()
-time.sleep(0.5) #これがないと上手く再生されません
-audio_placeholder.markdown(audio_html, unsafe_allow_html=True)
 
     
 #-----------------------------------------------------------------
@@ -111,6 +92,26 @@ else:
                 st.write("入眠に適しているといえる状態です")
         if past_skin_avg1 > current_skin_avg:
             st.write("皮膚温度が上がっていません")
+            audio_path1 = 'data/short-8bit-05.wav' #入力する音声ファイル
+
+            audio_placeholder = st.empty()
+
+            file_ = open(audio_path1, "rb")
+            contents = file_.read()
+            file_.close()
+
+            audio_str = "data:audio/ogg;base64,%s"%(base64.b64encode(contents).decode())
+            audio_html = """
+                      <audio autoplay=True>
+                      <source src="%s" type="audio/ogg" autoplay=True>
+                      Your browser does not support the audio element.
+                      </audio>
+                      """ %audio_str
+
+            audio_placeholder.empty()
+            time.sleep(0.5) #これがないと上手く再生されません
+            audio_placeholder.markdown(audio_html, unsafe_allow_html=True)
+          
            
 
 
