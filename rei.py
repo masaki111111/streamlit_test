c23f944f42acb5b5f1a4b5933cec1fff63e44bf6
masaki111111 <oit.e1921079@gmail.com>
2025-01-11 10:36:03 +0900

    rei.py

--- rei.py
+++ rei.py
@@ -39,6 +39,9 @@ except Exception as e:
 
 # 使用者に基準となる日時を入力してもらう
 # 日時の入力 (日付と時間を分けて入力)
+st.date_input("基準となる日付を選択してください")
+st.time_input("基準となる時刻を選択してください")
+
 date_input = st.date_input("基準となる日付を選択してください")
 time_input = st.time_input("基準となる時刻を選択してください")
 
