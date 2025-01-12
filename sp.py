a6579a72c3088b506e07ed67a0011cbd39f4a9b4
masaki111111 <oit.e1921079@gmail.com>
2025-01-12 16:56:23 +0900

    Update sp.py

--- sp.py
+++ sp.py
@@ -46,9 +46,9 @@ except Exception as e:
 #-----------------------------------起床のCORE----------------------------------------------
 # 6時以降のデータをフィルタリング（6時～9時の範囲）
 
-df_after_22 = df[(df['datetime'].dt.hour >= 6) ]
+df_after_6 = df[(df['datetime'].dt.hour >= 6) ]
 
-if df_after_22.empty:
+if df_after_6.empty:
     st.warning("22時以降のデータが見つかりません。")
 else:
     # データの確認
@@ -61,31 +61,31 @@ else:
         core_temp_6 = df_after_6.iloc[:, 2]  # 3列目（深部体温）
 
         # データが十分にあるか確認
-        if len(skin_temp_6) >= 16 and len(core_temp) >= 16:
+        if len(skin_temp_6) >= 16 and len(core_temp6) >= 16:
             # 過去10行と現在6行の平均を計算（皮膚温度）
-            past_skin_avg1 = skin_temp_6.iloc[-70:-60].mean()
-            past_skin_avg2 = skin_temp_6.iloc[-40:-30].mean()
-            current_skin_avg = skin_temp_6.iloc[-10:].mean()
+            past_skin_av1 = skin_temp_6.iloc[-70:-60].mean()
+            past_skin_av2 = skin_temp_6.iloc[-40:-30].mean()
+            current_skin_av = skin_temp_6.iloc[-10:].mean()
 
             # 過去10行と現在6行の平均を計算（深部体温）
-            past_core_avg1 = core_temp_6.iloc[-70:-60].mean()
-            past_core_avg2 = core_temp_6.iloc[-40:-30].mean()
-            current_core_avg = core_temp_6.iloc[-10:].mean()
+            past_core_av1 = core_temp_6.iloc[-70:-60].mean()
+            past_core_av2 = core_temp_6.iloc[-40:-30].mean()
+            current_core_av = core_temp_6.iloc[-10:].mean()
 
-            skinsub = past_skin_avg1 - current_skin_avg
+            skinsub = past_skin_av1 - current_skin_av
 
-            coresub = past_core_avg1 - current_core_avg
+            coresub = past_core_av1 - current_core_av
             
             # 平均値を表示
-            st.write(f"1時間前の平均皮膚温度: {past_skin_avg1:.2f}",f"。30分前の平均皮膚温度: {past_skin_avg2:.2f}",f"。現在の平均皮膚温度: {current_skin_avg:.2f}")
-            st.write(f"1時間前の平均深部体温: {past_core_avg1:.2f}",f"。30前の平均深部体温: {past_core_avg2:.2f}",f"。現在の平均深部体温: {current_core_avg:.2f}")
+            st.write(f"1時間前の平均皮膚温度: {past_skin_av1:.2f}",f"。30分前の平均皮膚温度: {past_skin_av2:.2f}",f"。現在の平均皮膚温度: {current_skin_av:.2f}")
+            st.write(f"1時間前の平均深部体温: {past_core_av1:.2f}",f"。30前の平均深部体温: {past_core_av2:.2f}",f"。現在の平均深部体温: {current_core_av:.2f}")
             st.write("")
-        if past_skin_avg1 < current_skin_avg:
-            if past_core_avg1 < current_core_avg:
+        if past_skin_av1 < current_skin_av:
+            if past_core_av1 < current_core_av:
                 st.write("皮膚温度が上がり始めていますが深部体温が下がっていません")
-            if past_core_avg1 > current_core_avg:
+            if past_core_av1 > current_core_av:
                 st.write("入眠に適しているといえる状態です")
-        if past_skin_avg1 > current_skin_avg:
+        if past_skin_av1 > current_skin_av:
             st.write("皮膚温度が上がっていません")
                 
             
@@ -99,19 +99,19 @@ else:
             st.caption("眠り始めの90分で、いかに深く、質の良い睡眠がとれるかで睡眠全体の質が変わります睡眠に影響を及ぼす体の温度には2種類あり、体の内部を指す「深部体温」は、睡眠中に下がり臓器や筋肉、脳などを休ませます。")
             st.caption("もう1つは「皮膚温度」です。目が覚めている時は通常、深部体温のほうが皮膚温度よりも2℃ほど高いです。そして、入眠前には皮膚温度が上昇し、手足がポカポカすることで放熱を行い、深部体温を下げ、皮膚温度と深部体温の差が2℃以下に縮まることで、黄金の90分が生まれます。")
             # 皮膚温度が下がり始めたタイミングを検出
-        if len(skin_temp) >= 16 and len(core_temp) >= 16:
+        if len(skin_temp_6) >= 16 and len(core_temp_6) >= 16:
             # 皮膚温度と深部体温の変化を検出
-            for i in range(len(skin_temp) - 1, 9, -1):
+            for i in range(len(skin_temp_6) - 1, 9, -1):
                 # 10行の平均を更新
-                past_skin_avg = skin_temp.iloc[i-70:i-60].mean()  # 過去70行-60(一時間前の10分平均)
-                current_skin_avg = skin_temp.iloc[i-10:i].mean()  # 現在10行
-                past_core_avg = core_temp.iloc[i-70:i-60].mean()  # 過去70行-60(一時間前の10分平均)
-                current_core_avg = core_temp.iloc[i-10:i].mean()  # 現在10行
+                past_skin_avg_6 = skin_temp_6.iloc[i-70:i-60].mean()  # 過去70行-60(一時間前の10分平均)
+                current_skin_avg_6 = skin_temp_6.iloc[i-10:i].mean()  # 現在10行
+                past_core_avg_6 = core_temp_6.iloc[i-70:i-60].mean()  # 過去70行-60(一時間前の10分平均)
+                current_core_avg_6 = core_temp_6.iloc[i-10:i].mean()  # 現在10行
 
-                # 皮膚温度が上昇し、深部体温が下降しているか判定
-                if current_skin_avg > past_skin_avg and current_core_avg < past_core_avg:
+                # 皮膚温度下降し、深部体温が上昇しているか判定
+                if current_skin_avg_6 < past_skin_avg_6 and current_core_avg_6 > past_core_avg_6:
                     # 皮膚温度が上がり、深部体温が下がり始めたタイミングの行を表示
-                    st.warning(f"皮膚温度が上昇し、深部体温が下降している時刻: {df_after_22.iloc[i]['datetime']}")
+                    st.warning(f"皮膚温度が上昇し、深部体温が下降している時刻: {df_after_6.iloc[i]['datetime']}")
                     break  # 最初に条件を満たしたタイミングで終了
 
             else:
