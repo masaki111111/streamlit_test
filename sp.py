77327aeb22173dda8617d31f3d82e3bdd069a3be
masaki111111 <oit.e1921079@gmail.com>
2025-01-12 18:03:52 +0900

    Update sp.py

--- sp.py
+++ sp.py
@@ -44,12 +44,12 @@ except Exception as e:
 
 
 #-----------------------------------起床のCORE----------------------------------------------
-# 6時以降のデータをフィルタリング（6時～9時の範囲）
+# 6時以降のデータをフィルタリング（6時～9時の範囲）この数字はユーザによって変更する
 
-df_after_6 = df[(df['datetime'].dt.hour >= 6) ]
+df_after_6 = df[(df['datetime'].dt.hour >= 6) | (df['datetime'].dt.hour < 12)]
 
 if df_after_6.empty:
-    st.warning("22時以降のデータが見つかりません。")
+    st.warning("6時以降のデータが見つかりません。")
 else:
     # データの確認
     st.write("6時以降のデータが表示されます:")
@@ -63,13 +63,17 @@ else:
         # データが十分にあるか確認
         if len(skin_temp_6) >= 16 and len(core_temp_6) >= 16:
             # 過去10行と現在6行の平均を計算（皮膚温度）
-            past_skin_av1 = skin_temp_6.iloc[-70:-60].mean()
-            past_skin_av2 = skin_temp_6.iloc[-40:-30].mean()
-            current_skin_av = skin_temp_6.iloc[-10:].mean()
+            past_skin_av1 = skin_temp_6.iloc[-70:-60].mean() #一時間前
+            past_skin_av2 = skin_temp_6.iloc[-40:-30].mean() #三十分前
+            past_skin_av3 = skin_temp_6.iloc[-20:-10].mean() #十分前
+            current_skin_av = skin_temp_6.iloc[-10:].mean() #現時点
 
             # 過去10行と現在6行の平均を計算（深部体温）
-            past_core_av1 = core_temp_6.iloc[-70:-60].mean()
-            past_core_av2 = core_temp_6.iloc[-40:-30].mean()
+            past_core_av1 = core_temp_6.iloc[-70:-60].mean() #1時間前
+            past_core_av2 = core_temp_6.iloc[-50:-40].mean() #40分前
+            past_core_av3 = core_temp_6.iloc[-40:-30].mean() #30分前
+            past_core_av4 = core_temp_6.iloc[-20:-10].mean() #10分前
+            past_core_av5 = core_temp_6.iloc[-15:-5].mean() #5分前
             current_core_av = core_temp_6.iloc[-10:].mean()
 
             skinsub = past_skin_av1 - current_skin_av
@@ -80,17 +84,46 @@ else:
             st.write(f"1時間前の平均皮膚温度: {past_skin_av1:.2f}",f"。30分前の平均皮膚温度: {past_skin_av2:.2f}",f"。現在の平均皮膚温度: {current_skin_av:.2f}")
             st.write(f"1時間前の平均深部体温: {past_core_av1:.2f}",f"。30前の平均深部体温: {past_core_av2:.2f}",f"。現在の平均深部体温: {current_core_av:.2f}")
             st.write("")
-        if past_skin_av1 < current_skin_av:
-            if past_core_av1 < current_core_av:
-                st.write("皮膚温度が上がり始めていますが深部体温が下がっていません")
-            if past_core_av1 > current_core_av:
-                st.write("入眠に適しているといえる状態です")
-        if past_skin_av1 > current_skin_av:
-            st.write("皮膚温度が上がっていません")
+            
+        #深部体温が1時間前,30分前に10分前に上がっている且つ,5分前と比べて下がっているときアラーム(下限のピーク検出)
+        if past_core_av1 > current_core_av2:
+            if past_core_av2 < past_core_av3:
+             if past_core_av3 < past_core_av4:
+        　　　 if past_core_av4 < current_core_av:
+                st.write("目覚めるのに最適な時間です")
+                audio_path1 = 'data/short-8bit-05.wav' #入力する音声ファイル
+
+                audio_placeholder = st.empty()
+
+                file_ = open(audio_path1, "rb")
+                contents = file_.read()
+                file_.close()
+
+                audio_str = "data:audio/ogg;base64,%s"%(base64.b64encode(contents).decode())
+                audio_html = """
+                          <audio autoplay=True>
+                          <source src="%s" type="audio/ogg" autoplay=True>
+                          Your browser does not support the audio element.
+                          </audio>
+                          """ %audio_str
+
+                audio_placeholder.empty()
+                time.sleep(0.5) #これがないと上手く再生されません
+                audio_placeholder.markdown(audio_html, unsafe_allow_html=True)
+
+          if past_core_av2 < current_core_av:
+              st.write("深部体温が上昇しています二度寝をせずにそのまま起きましょう")
+        else:
+            　st.write("目覚めはどうでしたか")
+            　st.caption("現在深部体温が十分に上がっていません")
+             
+         
+        
+        
                 
             
             #st.write(f"1時間前と現在の皮膚温の差: {skinsub:.2f}")       
-            #st.write(f"1時間前と現在の皮膚温の差: {coresub:.2f}")
+            #st.write(f"1時間前と現在の深部体温の差: {coresub:.2f}")
                      
             
 
