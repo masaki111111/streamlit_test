cacbe00eddb06a5400759d4261734ba3568d0d76
masaki111111 <oit.e1921079@gmail.com>
2025-01-16 09:29:40 +0900

    Update 実験1(C0).py

--- 実験1(C0).py
+++ 実験1(C0).py
@@ -23,7 +23,7 @@ import time
 
 #csv_file_path = "data/05_12_2024_DA38DDB3C43F_history.csv"
 #csv_file_path = "data/11_06_15_2024_DA38DDB3C43F_history.csv"
-csv_file_path = "data/16_1_1.csv"
+csv_file_path = "data/16_1_2.csv"
 
 
 try:
